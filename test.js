c6445511417c3242cd9d0b5cbf929be1e48c8c6b
Syed Ammad <syedammad@192.168.0.136>
2024-02-23 15:24:40 +0500

    ...

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
+const axios = require("axios").default;
 const books = require("./src/api/v1/db/books.json");
 const verseList = require("./src/api/v1/db/chapters-verse-list.json");
+const versionList = require("./src/api/v1/db/versions-list.json");
 
-books.forEach((e, i) => {
-  verseList[i].alias = e.aliase;
-});
+const fn = async () => {
+  console.log(verseList[0]);
+  const promises = [];
 
-console.log(JSON.stringify(verseList))
+  versionList.forEach((version) => {
+    verseList.forEach(({ chapters, alias }) => {
+      chapters.forEach(({ chapter }) => {
+        const url = `http://ec2-3-80-86-162.compute-1.amazonaws.com:3000/api/v1/verse-with-index?chapter=${chapter}&book=${alias}&version=${version.versionName}`;
+        promises.push(axios.get(url));
+      });
+    });
+  });
+
+  console.log(promises.length);
+  await Promise.all(promises)
+    .then((e) => console.log("Promise Finished...."))
+    .catch((e) => console.log("Got Error"));
+};
+
+fn();
